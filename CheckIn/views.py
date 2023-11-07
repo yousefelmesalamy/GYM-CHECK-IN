587c4987969f968b18fd95d62df9b07671989c2e
Yousef Elmesalamy <115108581+yousefelmesalamy@users.noreply.github.com>
2023-11-07 12:30:24 +0200

    urls edited

--- CheckIn/views.py
+++ CheckIn/views.py
@@ -1,30 +1,6 @@
-from django.contrib.auth.models import User
-from django.conf import settings
-from rest_framework.response import Response
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
-
 from .serializers import UserSerializer
-from rest_framework.views import APIView
 from .permissons import UserPermission
-from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
-from django.contrib.auth.signals import user_logged_in, user_logged_out
-from django.utils import timezone
-from rest_framework.serializers import DateTimeField
-from rest_framework.settings import api_settings
-# -------------------------------------------------------------------------------------------
-# Create your views here.
-# Knox
-from django.contrib.auth import login
-# from knox.views import LoginView as KnoxLoginView
-from rest_framework.authtoken.serializers import AuthTokenSerializer
-from rest_framework import permissions
-from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import render
 from .models import InMemberShip
 from .serializers import InMemberShipSerializer
 from django.shortcuts import get_object_or_404
@@ -34,6 +10,7 @@ from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from django.http import HttpResponse
 from .models import USER
+from django.shortcuts import render, redirect
 
 
 class InmemberShipViewSet(viewsets.ModelViewSet):
@@ -45,6 +22,7 @@ class InmemberShipViewSet(viewsets.ModelViewSet):
                      'membership_end_date',
                      'membership_days_of_week', 'membership_check_in_count', 'membership_check_in_count_remaining',
                      'membership_check_in_count_remaining']
+
 
 
 def check_in(request, subscriber_id):
@@ -59,9 +37,6 @@ def check_in(request, subscriber_id):
 
 def check_in_view(request, subscriber_id):
     return HttpResponse(f"Checking in subscriber with ID {subscriber_id}")
-
-
-from django.shortcuts import render, redirect
 
 
 def ask_for_id(request):
